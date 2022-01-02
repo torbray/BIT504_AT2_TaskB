aa06388d4a42845f324073249523a88b5895d646
torbray <torbray+github@gmail.com>
2022-01-03 02:16:42 +1300

    Created ImageViewer class to load single image viewer

--- ImageGallery/src/main/ImageGallery.java
+++ ImageGallery/src/main/ImageGallery.java
@@ -1,9 +1,32 @@
 package main;
 
-public class ImageGallery {
+import javax.swing.JFrame;
 
+import main.components.ImageViewer;
+
+public class ImageGallery extends JFrame {
+
+	private static final String WINDOW_TITLE = "Image Gallery";
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 600;
+	
+	// Constructor
+	public ImageGallery() {		
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		add(new ImageViewer());
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	// Entry point
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new ImageGallery();
+			}
+		});
 
 	}
 
---
+++ ImageGallery/src/main/components/ImageViewer.java
@@ -0,0 +1,14 @@
+package main.components;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class ImageViewer extends JPanel {
+	
+	
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+}
