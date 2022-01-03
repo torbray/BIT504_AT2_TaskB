305fa20fed02f6a5f7ce13bee548ad93116cfede
torbray <torbray+github@gmail.com>
2022-01-03 17:51:20 +1300

    Adding JLabel feature to load img, adding img of cat

--- ImageGallery/src/main/components/ImageViewer.java
+++ ImageGallery/src/main/components/ImageViewer.java
@@ -1,14 +1,62 @@
 package main.components;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ImageViewer extends JPanel {
 	
+	private BufferedImage myPicture;
+	private JLabel picLabel;
 	
+	int image_width = getWidth();
+	int image_height = (int)(getHeight() * 0.8);
+	
+	public ImageViewer() {
+		initImgButton();
+	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+	}
+	
+	public void initImgButton() {
+		JButton jbutton = new JButton("Load Image");
+		jbutton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadImage();
+				
+			}
+			
+		});
+		
+		add(jbutton);
+	}
+	
+	public void loadImage() {
+		String filepath = "src/main/imgs/Thinking-of-getting-a-cat.png";
+		
+		try {
+			myPicture = ImageIO.read(new File(filepath));
+			picLabel = new JLabel(new ImageIcon(myPicture));
+			
+			add(picLabel);
+			validate();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
